34970cfc6b57c67c2d6fe5e958a115db609b17a2
h3y6e <38322494+h3y6e@users.noreply.github.com>
2026-04-06 06:26:06 +0000

    fix: update formulae

--- Formula/skills.rb
+++ Formula/skills.rb
@@ -1,30 +1,30 @@
 class Skills < Formula
   desc "CLI tool to manage local agent skills"
   homepage "https://github.com/h3y6e/skills"
-  version "2026.3.1"
+  version "2026.4.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/h3y6e/skills/releases/download/v2026.3.1/skills_2026.3.1_darwin_arm64.tar.gz"
-      sha256 "6b18f44228ca9029551a27a5b7d886bbed1626073f5470015a2e5a9b0935ccb1"
+      url "https://github.com/h3y6e/skills/releases/download/v2026.4.2/skills_2026.4.2_darwin_arm64.tar.gz"
+      sha256 "346f7b1fed9f862fc330be0539a7dfc3cd0ddcf8f024c132c3bb1f0bfa9964ce"
     end
 
     on_intel do
-      url "https://github.com/h3y6e/skills/releases/download/v2026.3.1/skills_2026.3.1_darwin_amd64.tar.gz"
-      sha256 "c73b6e5eb9b21ad24ec30e2d1d64ed1c933902e0dcfcc9b99a4dd5c4560a3acc"
+      url "https://github.com/h3y6e/skills/releases/download/v2026.4.2/skills_2026.4.2_darwin_amd64.tar.gz"
+      sha256 "8fb1f0467fc02ffd8bf6a5b723f7403789002933e704de0d48ab2b66eb788cf7"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/h3y6e/skills/releases/download/v2026.3.1/skills_2026.3.1_linux_arm64.tar.gz"
-      sha256 "9e8bac2ab848c7dbdedddc61c1a65b9b809999fb9a48b54999e93c3f3410e3e8"
+      url "https://github.com/h3y6e/skills/releases/download/v2026.4.2/skills_2026.4.2_linux_arm64.tar.gz"
+      sha256 "55d2f04647403f66e5b9ff95d68c6735a1843a06e3d2fbbe6d5b45614c1c2e99"
     end
 
     on_intel do
-      url "https://github.com/h3y6e/skills/releases/download/v2026.3.1/skills_2026.3.1_linux_amd64.tar.gz"
-      sha256 "895ea05c8c59be7cc19cb25eb00f5d34e18e73e9dea5d8c03b1d4abbf5df666f"
+      url "https://github.com/h3y6e/skills/releases/download/v2026.4.2/skills_2026.4.2_linux_amd64.tar.gz"
+      sha256 "cb8774f2e4d7f7ffd8e232d3eecf5f824da3b200849c0670694961baac1fadd4"
     end
   end
 
